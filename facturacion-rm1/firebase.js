be8c1fe21b2f9c9f537d880a99d72fc3729cab9d
licaptio <licaptio42@gmail.com>
2026-01-14 15:51:25 -0600

    Update firebase.js

--- facturacion-rm1/firebase.js
+++ facturacion-rm1/firebase.js
@@ -15,9 +15,12 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROYECTO.firebaseapp.com",
-  projectId: "TU_PROYECTO",
+  apiKey: "AIzaSyCK5nb6u2CGRJ8AB1aPlRn54b97bdeAFeM",
+  authDomain: "inventariopv-643f1.firebaseapp.com",
+  projectId: "inventariopv-643f1",
+  storageBucket: "inventariopv-643f1.firebasestorage.app",
+  messagingSenderId: "96242533231",
+  appId: "1:96242533231:web:aae75a18fbaf9840529e9a"
 };
 
 export const app = initializeApp(firebaseConfig);
