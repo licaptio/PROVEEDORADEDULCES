e411c774263c10c5e56359d5bae2083affe586fa
licaptio <licaptio42@gmail.com>
2026-01-14 18:50:42 -0600

    Create configFiscal.js

---
+++ facturacion-rm1/sifei/configFiscal.js
@@ -0,0 +1,20 @@
+// sifei/configFiscal.js
+export const FISCAL_EMISOR = {
+  rfc: "PDD031204KL5",
+  razonSocial: "PROVEEDORA DE DULCES Y DESECHABLES",
+  regimenFiscal: "601",
+  cpExpedicion: "67700",
+  numeroCertificado: "00001000000716399644",
+
+  // Datos extendidos (opcionales pero reales en SIFEI)
+  direccionEmisor: "MADERO 690 CENTRO LINARES NUEVO LEON MEXICO",
+  emailEmisor: ""
+};
+
+export const RECEPTOR_PUBLICO_GENERAL = {
+  rfc: "XAXX010101000",
+  nombre: "PUBLICO EN GENERAL",
+  usoCFDI: "S01",
+  regimenFiscal: "616",
+  cp: "67700"
+};
