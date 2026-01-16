f14f4633e18b1cf994ecf3260d88293a1330db83
licaptio <licaptio42@gmail.com>
2026-01-15 18:02:05 -0600

    Update factura-global-r1.js

--- facturacion-rm1 global/factura-global-r1.js
+++ facturacion-rm1 global/factura-global-r1.js
@@ -10,9 +10,12 @@ import {
 
 // 🔥 TU CONFIG REAL AQUÍ
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_AUTH",
-  projectId: "TU_PROJECT_ID"
+  apiKey: "AIzaSyCK5nb6u2CGRJ8AB1aPlRn54b97bdeAFeM",
+  authDomain: "inventariopv-643f1.firebaseapp.com",
+  projectId: "inventariopv-643f1",
+  storageBucket: "inventariopv-643f1.firebasestorage.app",
+  messagingSenderId: "96242533231",
+  appId: "1:96242533231:web:aae75a18fbaf9840529e9a"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -84,3 +87,4 @@ async function cargarVentas() {
   cnt.textContent = c;
   totalEl.textContent = total.toFixed(2);
 }
+
