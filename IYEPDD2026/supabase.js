212ece64e76f98cae2ae1b365582f26950094dda
licaptio <licaptio42@gmail.com>
2026-01-20 18:30:50 -0600

    Update supabase.js

--- IYEPDD2026/supabase.js
+++ IYEPDD2026/supabase.js
@@ -2,7 +2,9 @@
 const SUPABASE_URL = 'https://cvpbtjlupswbyxenugpz.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN2cGJ0amx1cHN3Ynl4ZW51Z3B6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc3MDIxOTQsImV4cCI6MjA2MzI3ODE5NH0.iiJsYM3TtaGPdeCtPcEXwAz3LfFc1uJGECEvOErvrqY';
 
-window.supabase = supabase.createClient(
+window.supabaseClient = window.supabaseClient || null;
+
+window.supabaseClient = supabase.createClient(
   SUPABASE_URL,
   SUPABASE_KEY
 );
