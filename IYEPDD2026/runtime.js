a5ca5d548799df7a034489d26f78442e16b9bc9a
licaptio <licaptio42@gmail.com>
2026-01-25 09:25:28 -0600

    Update runtime.js

--- IYEPDD2026/runtime.js
+++ IYEPDD2026/runtime.js
@@ -1,5 +1,5 @@
 // runtime.js
 window.RUNTIME = {
-  endpoint: 'https://TU_PROYECTO.supabase.co',
-  key: 'TU_ANON_KEY'
+  endpoint: 'https://cvpbtjlupswbyxenugpz.supabase.co',
+  key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN2cGJ0amx1cHN3Ynl4ZW51Z3B6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc3MDIxOTQsImV4cCI6MjA2MzI3ODE5NH0.iiJsYM3TtaGPdeCtPcEXwAz3LfFc1uJGECEvOErvrqY'
 };
