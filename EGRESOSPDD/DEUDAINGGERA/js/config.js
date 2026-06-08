12f140efc9700b7eb97ee2e303dc7b29d6dd5a7c
licaptio <licaptio42@gmail.com>
2026-06-08 00:02:06 -0600

    Add files via upload

---
+++ EGRESOSPDD/DEUDAINGGERA/js/config.js
@@ -0,0 +1,11 @@
+export const supabaseUrl = "https://cvpbtjlupswbyxenugpz.supabase.co";
+
+export const supabaseAnonKey =
+  "sb_publishable_SQ7Q5LFJqlxVzwNTxcIyzQ_8F1bqyiX";
+
+export const APP_CONFIG = {
+  nombre: "PROVSOFT",
+  modulo: "Control de Facturas en Observación",
+  usuarioDefault: "GERARDO",
+  logo: "./assets/logo.png"
+};
