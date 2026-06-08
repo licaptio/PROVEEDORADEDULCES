9c75d53ddd8294d67dd754dc15ad7222bb63dad2
licaptio <licaptio42@gmail.com>
2026-06-08 00:04:51 -0600

    Update config.js

--- EGRESOSPDD/DEUDAINGGERA/js/config.js
+++ EGRESOSPDD/DEUDAINGGERA/js/config.js
@@ -1,3 +1,12 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCK5nb6u2CGRJ8AB1aPlRn54b97bdeAFeM",
+  authDomain: "inventariopv-643f1.firebaseapp.com",
+  projectId: "inventariopv-643f1",
+  storageBucket: "inventariopv-643f1.firebasestorage.app",
+  messagingSenderId: "96242533231",
+  appId: "1:96242533231:web:aae75a18fbaf9840529e9a"
+};
+
 export const supabaseUrl = "https://cvpbtjlupswbyxenugpz.supabase.co";
 
 export const supabaseAnonKey =
@@ -7,5 +16,6 @@ export const APP_CONFIG = {
   nombre: "PROVSOFT",
   modulo: "Control de Facturas en Observación",
   usuarioDefault: "GERARDO",
-  logo: "./assets/logo.png"
+  logo: "./assets/logo.jfif",
+  correoAutorizado: "licaptio42@gmail.com"
 };
