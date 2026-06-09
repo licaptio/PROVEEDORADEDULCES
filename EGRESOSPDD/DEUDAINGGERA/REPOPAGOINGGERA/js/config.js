51f96eb49e3e062b3fd3167c40080fd7cd0407ba
licaptio <licaptio42@gmail.com>
2026-06-08 18:49:48 -0600

    Update config.js

--- EGRESOSPDD/DEUDAINGGERA/REPOPAGOINGGERA/js/config.js
+++ EGRESOSPDD/DEUDAINGGERA/REPOPAGOINGGERA/js/config.js
@@ -1,5 +1,5 @@
 window.PROVSOFT_CONFIG = {
-  SUPABASE_URL: 'https://cvpbtjlupswbyxenugpz.supabase.co/rest/v1/',
+  SUPABASE_URL: 'https://cvpbtjlupswbyxenugpz.supabase.co',
   SUPABASE_ANON_KEY: 'sb_publishable_SQ7Q5LFJqlxVzwNTxcIyzQ_8F1bqyiX',
   TABLA_PAGOS: 'pagos_proveedor',
   INICIO_SEMANA: 1,
