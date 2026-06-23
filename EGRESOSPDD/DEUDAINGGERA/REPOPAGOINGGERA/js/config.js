5361230b5a8f49331ebed072247856f3abd98f78
licaptio <licaptio42@gmail.com>
2026-06-23 11:08:30 -0600

    Delete EGRESOSPDD/DEUDAINGGERA directory

--- EGRESOSPDD/DEUDAINGGERA/REPOPAGOINGGERA/js/config.js
+++
@@ -1,7 +0,0 @@
-window.PROVSOFT_CONFIG = {
-  SUPABASE_URL: 'https://cvpbtjlupswbyxenugpz.supabase.co',
-  SUPABASE_ANON_KEY: 'sb_publishable_SQ7Q5LFJqlxVzwNTxcIyzQ_8F1bqyiX',
-  TABLA_PAGOS: 'pagos_proveedor',
-  INICIO_SEMANA: 1,
-  NOMBRE_REPORTE: 'Reporte semanal de pagos a proveedores'
-};
--- EGRESOSPDD/DEUDAINGGERA/REPOPAGOINGGERA/js/modales.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('click', e=>{
-  const cerrar=e.target.dataset?.cerrar;
-  if(cerrar) cerrarModal(cerrar);
-  if(e.target.classList.contains('modal')) e.target.classList.add('oculto');
-});
