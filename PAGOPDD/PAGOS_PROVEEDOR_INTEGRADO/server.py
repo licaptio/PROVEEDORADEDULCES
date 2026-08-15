4912cbe02a6b14510eaaa6ec4e1e392a33fddd0b
licaptio <licaptio42@gmail.com>
2026-08-15 13:17:52 -0600

    Delete PAGOPDD/PAGOS_PROVEEDOR_INTEGRADO directory

--- PAGOPDD/PAGOS_PROVEEDOR_INTEGRADO/server.py
+++
@@ -1,9 +0,0 @@
-from http.server import ThreadingHTTPServer, SimpleHTTPRequestHandler
-import os, webbrowser
-
-PORT = 8000
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-url = f"http://localhost:{PORT}/"
-print(f"PROVSOFT - Pagos a Proveedores\nAbriendo {url}\nCTRL+C para cerrar.")
-webbrowser.open(url)
-ThreadingHTTPServer(("127.0.0.1", PORT), SimpleHTTPRequestHandler).serve_forever()
