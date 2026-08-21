3dd68ba8674d4977d0674ee32ebd348798b0f186
licaptio <licaptio42@gmail.com>
2026-08-21 17:06:08 -0600

    Delete PAGOPDD/TAUROPAGO directory

--- PAGOPDD/TAUROPAGO/assets/js/config.js
+++
@@ -1,4 +0,0 @@
-export const RFC_TAURO = "BTA010327AD3";
-export const NOMBRE_TAURO = "TAURO";
-export const SUPABASE_URL = "https://cvpbtjlupswbyxenugpz.supabase.co";
-export const SUPABASE_KEY = "sb_publishable_SQ7Q5LFJqlxVzwNTxcIyzQ_8F1bqyiX";
